14c76ccd66d344e109cb51e8694b37072edec040
Alazar G <alazar4018@gmail.com>
2020-10-17 06:56:46 +0300

    increase gc threshhold.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -27,9 +27,26 @@
 (setq auto-save-file-name-transforms
           `((".*" ,temporary-file-directory t)))
 
+;; gui related stuff.
+
+					; no menubar
+(menu-bar-mode -1)
+					; disable scrollbar
+(toggle-scroll-bar -1)
+					; disable toolbar
+(tool-bar-mode -1)
+					; no welcome page
+(setq inhibit-startup-message t)
+
+;; lsp settings
+(setq gc-cons-threshhold 10000000)
+(setq lsp-enable-semantic-highlighting t)
+
 ;; rust related settings.
 
 (setq lsp-rust-analyzer-server-command "rust-analyzer")
+(add-hook 'before-save-hook (lambda () (when (eq 'rust-mode major-mode)
+                                           (lsp-format-buffer))))
 
 
 (custom-set-variables
@@ -37,7 +54,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(lsp-mode company rust-mode use-package)))
+ '(package-selected-packages
+   '(glsl-mode dap-mode lsp-mode company rust-mode use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
