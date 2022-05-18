81f095b91e6c5ef63ca4726672c6c5e2185f81cc
Alazar G <alazar4018@gmail.com>
2022-05-18 22:02:18 +0300

    set fuzzel as the application launcher.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,2 +1,10 @@
--- vim set jk to escape.
+-- vim set jk to escape for insert mode.
 vim.keymap.set("i", "jk", "<Esc>")
+
+-- set Esc to enter normal mode in terminal.
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
+
+-- set tabsize to 4.
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
